e18e9c2121f9f74fd2e8a9f87677557386d9172a
Дмитрий Дятчин <d.dyatchin@dom.ibzkh.ru>
2021-02-24 10:33:31 +0300

    upload v2

---
+++ folder/anyFile.js
@@ -0,0 +1,6 @@
+const anyFile = (props) => {
+    return <h1> Hello form anyFile </h1>
+}
+
+
+export default anyFile;
--- reactLazy.js
+++ reactLazy.js
@@ -1,3 +1,8 @@
+import anyFile from './folder/anyFile'
+
 const reactLazy = (props) => {
-    return <h1> Hello from ReactLazy Component </h1>
+    return <> 
+        <h1> Hello from ReactLazy Component </h1>
+        <anyFile />
+    </>
 }
